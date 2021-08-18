99e04292734a9c01d10ff5e5bae073819d0d4e2c
sandhyakopparla <sandhyakopparla2413@gmail.com>
2021-08-18 15:38:05 +0530

    submited all the programs

---
+++ day13_4Aug/myfile.py
@@ -0,0 +1,11 @@
+# myfile=open('demo.txt','w')  
+# myfile.write("hello world")
+# myfile=open('file1.txt','r')
+myfile=open('demo.txt','w')  
+myfile.write("india")
+myfile=open('file.txt','r')
+x=myfile.read()
+print(x)
+
+#myfile.close()
+
